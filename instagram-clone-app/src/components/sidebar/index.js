a9d90758a7d54e8fd825509cfaf80a859a53dba6
Ryan Watson <rwatson1799@gmail.com>
2021-04-25 09:50:17 +1000

    Create basis for the sidebar components

---
+++ instagram-clone-app/src/components/sidebar/index.js
@@ -0,0 +1,14 @@
+import useUser from '../../hooks/use-user';
+import User from './user';
+import Suggestions from './suggestions';
+
+export default function Sidebar() {
+  const { user: { fullname, username, userId } } = useUser();
+
+  return (
+    <div className="p-4">
+      <User username={username} fullname={fullname} />
+      <Suggestions userId={userId} />
+    </div>
+  );
+}
