6532a221cae83788e9a3190751b135eebc78a2d7
Ryan Watson <rwatson1799@gmail.com>
2021-04-24 23:47:15 +1000

    Start dashboard and complete header

--- instagram-clone-app/src/App.js
+++ instagram-clone-app/src/App.js
@@ -6,6 +6,7 @@ import useAuthListener from './hooks/use-auth-listener';
 
 const Login = lazy(() => import('./pages/login'));
 const SignUp = lazy(() => import('./pages/sign-up'));
+const Dashboard = lazy(() => import('./pages/dashboard'));
 const NotFound = lazy(() => import('./pages/not-found'));
 
 export default function App() {
@@ -18,6 +19,7 @@ export default function App() {
           <Switch>
             <Route path={ROUTES.LOGIN} component={Login} />
             <Route path={ROUTES.SIGN_UP} component={SignUp} />
+            <Route path={ROUTES.DASHBOARD} component={Dashboard} />
             <Route component={NotFound} />
           </Switch>
         </Suspense>
